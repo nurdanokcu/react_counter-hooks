727b046f868c3044bf9a903ed01145bf67aca308
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-01-18 15:17:15 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 export const App: React.FC = () => {
-  const count = 0;
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // increase count by 1
+    setCount(current => current + 1);
   };
 
   const add100 = () => {
-    // increase count by 100
+    setCount(current => current + 100);
   };
 
   // DON'T change the code below
